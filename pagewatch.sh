43242d58dad6f3f4c0c48b227d458ea1bee90f8b
Areldyb <areldyb@gmail.com>
2021-02-01 16:58:39 -0600

    Add files via upload

--- pagewatch.sh
+++ pagewatch.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 # pagewatch.sh - Monitors a web page for changes
-# Requires lynx
+# Captures page using headless chromium, parses and simplifies with lynx, then prints any changes to console
+# Why chromium first, instead of just lynx? Javascript support.
 
 # Usage: ./pagewatch.sh [url] [check interval]
 
-# Original code shamelessly borrowed from bhfsteve: http://bhfsteve.blogspot.com/2013/03/monitoring-web-page-for-changes-using.html
+# TODO add flags: debug/verbose, quiet/don't-print-diff-results, bell
+# TODO add option to screen out changes that don't include spaces (basic attempt at finding natural language changes only)
+# TODO make sure the headless chromium is actually running javascript. :/
+
+# Care for Mother Earth, use recycled code!
 
 if [ $# -lt 2 ]; then
 	echo "Usage: ./pagewatch.sh [url] [check interval]"
-	exit
+	exit 1
 fi
 
-URLHASH=$(echo -n $1 | sha1sum | cut -d" " -f1)
+TARGET=$1
+INTERVAL=$2
+URLHASH=$(echo -n $TARGET | sha1sum | cut -d" " -f1)  # allows for running multiple pagewatch instances covering different pages without issue
+TEMPDIR=pagewatch-working
 
-USERAGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0"
-# some websites don't like lynx. yeah, we're totally firefox on win10
+function getthepage {
+	chromium --headless --disable-gpu --dump-dom $TARGET > $TEMPDIR/$URLHASH.html
+	lynx --dump --nolist $TEMPDIR/$URLHASH.html > $TEMPDIR/$URLHASH-new.txt 2>/dev/null
+}
 
-echo Getting a first look at $1
-#curl $1 -L --compressed -s > pagewatch-$URLHASH-new.temp
-lynx --dump --nolist --useragent="$USERAGENT" $1 > pagewatch-$URLHASH-new.temp 2>/dev/null
+# main
+mkdir -p $TEMPDIR
+echo Getting a first look at $TARGET
+getthepage
 echo Watching...
 for (( ; ; )); do	# for(ever) should be valid syntax, just sayin
-	sleep $2
-	mv pagewatch-$URLHASH-new.temp pagewatch-$URLHASH-old.temp 2>/dev/null
-	#curl $1 -L --compressed -s > pagewatch-$URLHASH-new.temp
-	lynx --dump --nolist --useragent="$USERAGENT" $1 > pagewatch-$URLHASH-new.temp 2>/dev/null
-	DIFF_OUTPUT="$(diff pagewatch-$URLHASH-old.temp pagewatch-$URLHASH-new.temp)"
+	sleep $INTERVAL
+	mv $TEMPDIR/$URLHASH-new.txt $TEMPDIR/$URLHASH-old.txt 2>/dev/null
+	getthepage
+	DIFF_OUTPUT="$(diff $TEMPDIR/$URLHASH-old.txt $TEMPDIR/$URLHASH-new.txt)"
 	if [ "0" != "${#DIFF_OUTPUT}" ]; then
 		echo CHANGE DETECTED $(date)
-		diff --color=always pagewatch-$URLHASH-old.temp pagewatch-$URLHASH-new.temp
+		diff --color=auto $TEMPDIR/$URLHASH-old.txt $TEMPDIR/$URLHASH-new.txt
 	fi
 done
